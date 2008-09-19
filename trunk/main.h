0dee219baa6b7dd3956b9fc042b50590045e4699
domas.m <domas.m@c4f24f84-7f43-11dd-ac5a-f353d88c4ef8>
2008-09-19 11:24:12 +0000

    main frame

--- trunk/main.h
+++ trunk/main.h
@@ -1,15 +1,5 @@
 #ifndef PMGAME_MAIN
 #define PMGAME_MAIN
 
-enum
-{
-    ID_Quit = 1,
-    ID_About,
-};
-
-BEGIN_EVENT_TABLE(PMMainFrame, wxFrame)
-    EVT_MENU(ID_Quit, PMMainFrame::OnQuit)
-    EVT_CUSTOM(WM_USER+1, 0, PMMainFrame::OnTest)
-END_EVENT_TABLE()
 
 #endif
