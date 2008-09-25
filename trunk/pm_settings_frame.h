879e9fde0469282ab9456fb846c3248526533b13
domas.m <domas.m@c4f24f84-7f43-11dd-ac5a-f353d88c4ef8>
2008-09-25 06:30:09 +0000

    It beeps!

--- trunk/pm_settings_frame.h
+++ trunk/pm_settings_frame.h
@@ -6,13 +6,16 @@
 #include "wx/textctrl.h"
 #include "wx/spinctrl.h"
 
-
 class PMSettings : public wxDialog {
 private:
     int teamCount;
-    wxTextCtrl * teamsDisplay[MAX_TEAMS];
     wxSizer * topSizer;
-    wxSpinCtrl * countDisplay;
+    wxSpinCtrl * countDisplay; //komandu sk.
+    wxTextCtrl * teamsDisplay[MAX_TEAMS]; //komandu pavadinimai    
+    //signalo ilgio ribos
+    wxTextCtrl * beepIntStart; 
+    wxTextCtrl * beepIntEnd;
+    wxCheckBox * beepRandom; //ar signalo trukmë parenkama atsitiktinai?
     
 public:
     PMSettings(wxWindow * parent, wxWindowID = wxID_ANY);  
@@ -22,10 +25,18 @@ public:
     void OnCountChange(wxSpinEvent & event);
     void OnCancel(wxCommandEvent & event);
     void OnOk(wxCommandEvent & event);
+    void OnRandomChange(wxCommandEvent & event); 
+    
+    void UpdateRandom();
     
     //setters & getters
     void SetCount(int count, bool set = false);    
     int GetCount();
+    
+    bool GetRandom();
+    void SetRandom(bool value);
+    void GetBeepLimits(int& min, int& max);
+    void SetBeepLimits(int min, int max);
     
     void SetName(int pos, wxString & label);
     wxString GetName(int pos);
